711d46b231312a21f1a8532d80df2d61b775c3c1
pandepic <39251913+pandepic@users.noreply.github.com>
2021-04-24 11:28:29 +1000

    base project setup

---
+++ LudumDare48/Source/Game.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using ElementEngine;
+
+namespace LudumDare48
+{
+    public enum GameStateType
+    {
+        Menu,
+    }
+    
+    public class Game : BaseGame
+    {
+        public Dictionary<GameStateType, GameState> GameStates { get; set; } = new Dictionary<GameStateType, GameState>();
+
+        public override void Load()
+        {
+            SettingsManager.LoadFromPath("Settings.xml");
+            
+            var windowRect = new ElementEngine.Rectangle()
+            {
+                X = 100,
+                Y = 100,
+                Width = SettingsManager.GetSetting<int>("Window", "Width"),
+                Height = SettingsManager.GetSetting<int>("Window", "Height")
+            };
+            
+            var vsync = false;
+#if DEBUG
+            vsync = false;
+#endif
+            
+            SetupWindow(windowRect, "LudumDare48", null, vsync);
+            SetupAssets("Content");
+            
+            ClearColor = Veldrid.RgbaFloat.CornflowerBlue;
+            
+            InputManager.LoadGameControls();
+            Window.Resizable = false;
+            
+            GameStates.Add(GameStateType.Menu, new GameStateMenu(this));
+        }
+        
+        public void SetGameState(GameStateType type)
+        {
+            if (GameStates.TryGetValue(type, out var state))
+                SetGameState(state);
+        }
+    }
+}
---
+++ LudumDare48/Source/GameStates/GameStateMenu.cs
@@ -0,0 +1,22 @@
+using ElementEngine;
+
+namespace LudumDare48
+{
+    public class GameStateMenu : GameState
+    {
+        public Game Game;
+        
+        public GameStateMenu(Game game)
+        {
+            Game = game;
+        }
+
+        public override void Update(GameTimer gameTimer)
+        {
+        }
+
+        public override void Draw(GameTimer gameTimer)
+        {
+        }
+    }
+}
