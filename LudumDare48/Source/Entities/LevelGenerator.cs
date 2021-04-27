838c9f86ffe6fd59a46f321bfb30cfd340d381a6
Jean-David Moisan <vynecompiler@gmail.com>
2021-04-26 20:52:51 -0400

    Reorder sounds

--- LudumDare48/Source/Entities/LevelGenerator.cs
+++ LudumDare48/Source/Entities/LevelGenerator.cs
@@ -12,48 +12,48 @@ namespace LudumDare48
         public string Asset;
         public int Index;
     }
-    
+
     public static class LevelGenerator
     {
         public static List<RecordingData> Recordings = new List<RecordingData>()
         {
             new RecordingData()
             {
-                Asset = "Forecast.ogg",
+                Asset = "One Missed Message.ogg",
                 Index = 1,
             },
-            
-            new RecordingData()
-            {
-                Asset = "One Missed Message.ogg",
-                Index = 2,
-            },
-            
+
             new RecordingData()
             {
                 Asset = "First Message.ogg",
+                Index = 2,
+            },
+
+            new RecordingData()
+            {
+                Asset = "Forecast.ogg",
                 Index = 3,
             },
-            
+
             new RecordingData()
             {
                 Asset = "Car Broke Down.ogg",
                 Index = 4,
             },
-            
+
             new RecordingData()
             {
                 Asset = "No Permit.ogg",
                 Index = 5,
             },
-            
+
             new RecordingData()
             {
                 Asset = "Farewell.ogg",
                 Index = 6,
             },
         };
-        
+
         public static void GenerateLevel(GameStatePlay gameState)
         {
             var rng = new Random(1);
@@ -62,25 +62,25 @@ namespace LudumDare48
             var jumpHeight = 100;
             var jumpLength = 200;
             var recordingIndex = 0;
-            
+
             var prevPlatformPosition = Vector2.Zero;
             var nextPlatformPosition = new Vector2(25, 500);
             var prevMovingPlatform = false;
-            
+
             for (var i = 0; i < platforms; i++)
             {
                 var platform = EntityBuilder.CreatePlatform(nextPlatformPosition, PlatformType.Normal);
                 ref var drawable = ref platform.GetComponent<DrawableMaskComponent>();
-                
+
                 var drawRect = EntityUtility.GetEntityDrawRect(platform);
                 if (drawRect.Bottom > gameState.DeathHeight)
                     gameState.DeathHeight = drawRect.Bottom;
-                
+
                 var platformSize = drawable.AtlasRect.SizeF * drawable.Scale;
                 var verticalDirection = rng.Next(0, 10) < 5 ? -1 : 1;
                 var platformOffset = new Vector2(platformSize.X + jumpLength, verticalDirection * (platformSize.Y + jumpHeight));
                 var recordingPlatform = false;
-                
+
                 if (i > 0 && i % platformsPerRecording == 0 && recordingIndex < Recordings.Count) {
                     var data = Recordings[recordingIndex];
                     var recording = EntityBuilder.CreateRecording(data.Asset, drawRect.LocationF, drawRect.LocationF + new Vector2(drawRect.Width / 2 - 100, -50f), recordingIndex == Recordings.Count - 1);
@@ -88,23 +88,23 @@ namespace LudumDare48
                     drawable.Color = Veldrid.RgbaFloat.Green;
                     recordingPlatform = true;
                 }
-                
+
                 if (i > 3 && rng.Next(0, 10) < 3)
                 {
                     var secondVerticalDirection = prevPlatformPosition.Y < nextPlatformPosition.Y ? 1 : -1;
-                    
+
                     var secondPlatformPosition = prevPlatformPosition
                         + new Vector2(
                             (platformSize.X + jumpLength) / 2,
                             ((platformSize.Y + jumpHeight) / 2) * secondVerticalDirection);
-                    
+
                     EntityBuilder.CreatePlatform(secondPlatformPosition, PlatformType.Death);
                     prevMovingPlatform = false;
                 }
                 else if (i > 3 && rng.Next(0, 10) < 3 && !prevMovingPlatform && !recordingPlatform)
                 {
                     var endPosition = nextPlatformPosition + new Vector2(platformOffset.X * 2f, 0);
-                    
+
                     platform.TryAddComponent(new MovingPlatformComponent()
                     {
                         StartPosition = nextPlatformPosition,
@@ -114,10 +114,10 @@ namespace LudumDare48
                         BaseCooldown = 2f,
                         Cooldown = 2f,
                     });
-                    
+
                     ref var collider = ref platform.GetComponent<ColliderComponent>();
                     collider.EventType = ColliderEventType.MovingPlatform;
-                    
+
                     nextPlatformPosition = endPosition;
                     prevMovingPlatform = true;
                 }
@@ -125,13 +125,13 @@ namespace LudumDare48
                 {
                     prevMovingPlatform = false;
                 }
-                
+
                 prevPlatformPosition = nextPlatformPosition;
                 nextPlatformPosition += platformOffset;
             }
-            
+
             gameState.DeathHeight += 500;
-            
+
         } // GenerateLevel
     } // LevelGenerator
 }
